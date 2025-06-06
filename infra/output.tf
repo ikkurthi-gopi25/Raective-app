c362e3c048465d6c8219dd7ae4878ee733631a3e
ikkurthi-gopi25 <gopiikkurthi425@gmail.com>
2025-06-06 15:46:43 +0530

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -1,4 +1,9 @@
-# Output the Public IP address
-output "ip_address" {
-  value = aws_instance.apache2_server.public_ip
+output "ec2_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.app_repo.repository_url
 }
